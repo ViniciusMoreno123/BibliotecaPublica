f4c365dea095287dc7a3de9cb17e98e581fb7fd3
ViniciusMoreno123 <vinisrocha8@gmail.com>
2022-07-19 20:45:46 -0400

    aula 69 leitor de qr Code

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,13 +1,56 @@
 import React, {Component} from "react";
-import {StyleSheet,Text,View} from "react-native"
+import {StyleSheet,Text,View,TouchableOpacity} from "react-native"
+import { BarCodeScanner } from "expo-barcode-scanner";
+import * as Permissions from "expo-permissions";
 
 export default class TransactionScreen extends Component {
+    constructor(props){
+    super(props);
+    this.state = {
+    domState:"normal",
+    hasCameraPermissions:null,
+    scanned:false,
+    scannedData:""
+    }
+    }
+
+    getCameraPermissions = async domState => {
+    const {status} = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+    hasCameraPermissions: status === "granted",
+    domState: domState,
+    scanned:false
+    })
+    }
+    handleBarCodeScanned = async ({type,data}) =>{
+        this.setState({
+            scannedData:data,
+            domState: "normal",
+            scanned:true
+        }) 
+    }
     render(){
+        const {domState,hasCameraPermissions,scannedData,scanned} = this.state;
+        if(domState === "scanner"){
+            return(
+                <BarCodeScanner onBarCodeScanned={scanned ? undenifed : this.handleBarCodeScanned}
+                style ={StyleSheet.absoluteFillObject}/>
+            )
+        }
+         
+        
         return(
         <View style={styles.container}>
-        <Text styles={styles.text}>
-         Tela De Transação
+        <Text style={styles.text}>
+        {hasCameraPermissions? scannedData : "Solicitar Permissão para a Camera"}
+        </Text >
+       <TouchableOpacity style ={styles.button} onPress ={() => this.getCameraPermissions("scanner")}>
+       
+        <Text styles={styles.buttonText}>
+         Digitalizar QR code 
         </Text>
+        </TouchableOpacity>
         </View>
         )
     }
@@ -19,10 +62,23 @@ const styles = StyleSheet.create({
   justifyContent:"center",
   alignItems:"center",
   backgroundColor:"#5653D4"
+
  },
- text:{
+ buttonText:{
  color: "#fff",
  fontSize:30,
- 
+
+ },
+ button:{
+ width:"43%",
+ height:80,
+ justifyContent:"center",
+ alignItems:"center",
+ backgroundColor:"#F48D20",
+ borderRadius:15
+ },
+ text:{
+    color:"#fff",
+    fontSize:25
  }
 })
