9471c8ab3705c2576cd2254cc9b1f853da466efc
ViniciusMoreno123 <vinisrocha8@gmail.com>
2022-07-21 21:06:23 -0400

    0.03 Customização de telas e icones

--- Components/BottomTabNavigator.js
+++ Components/BottomTabNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import {createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import TransactionScreen from "../screens/Transaction";
 import SearchScreen from "../screens/Search";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 var tab = createBottomTabNavigator();
 
@@ -10,7 +11,50 @@ export default class BottomTabNavigator extends Component{
   render(){
     return(
     <NavigationContainer>
-     <tab.Navigator>
+     <tab.Navigator
+     screenOptions={({route }) =>({
+      tabBarIcon:({focused, color, size}) =>{
+      let iconName;
+      if(route.name === "Transação"){
+      iconName = "book";
+      }else if (route.name === "Pesquisa"){
+      iconName = "search";
+      }
+      return (
+      <Ionicons
+      name={iconname}
+      size={size}
+      color={color}
+      />
+      )
+
+      }
+     })}
+     tabBarOptions={{
+      activeTintColor: "black",
+      inactiveTintColor: "#FFFFFF",
+      style: {
+        height: 130,
+        borderTopWidth: 0,
+        backgroundColor: "#5653d4"
+      },
+      labelStyle: {
+        fontSize: 20,
+        fontFamily: "Rajdhani_600SemiBold"
+      },
+      labelPosition: "beside-icon",
+      tabStyle: {
+        marginTop: 25,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 30,
+        borderWidth: 2,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#5653d4"
+      }
+     }}
+     >
     <tab.Screen   name= "Transação" component={TransactionScreen}/>
     <tab.Screen   name= "Pesquisa" component={SearchScreen}/>
 
